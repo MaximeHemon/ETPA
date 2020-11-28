753297a088eab7c8adaacc9aa0df69c7a86f5461
MaximeHemon <74669577+MaximeHemon@users.noreply.github.com>
2020-11-28 16:59:02 +0100

    Add files via upload

---
+++ TicTacToe.py
@@ -0,0 +1,73 @@
+grille = 
+def initialiseGrille (grille) 
+    compteur = 0 
+    for compteur in range (0,8) 
+    grille[compteur]= " " 
+    return()
+
+def afficher_grille("):
+    for i in range (3):
+        print(grille(i*3), grille(i*3+1), grille(i*3+2)) 
+        
+
+def ajouteSymbole:
+
+def testeVictoireVerticale:
+
+def testeVictoireHorizontale:
+
+def testeVictoireDiagonale:
+
+def testeJeuNul:
+
+#TicTaToe Number2
+grille =['*','*','*','*','*','*','*','*','*',]
+print("0 1 2")
+print("3 4 5")
+print("6 7 9")
+    
+    for compteur in range (0,8),
+        grille(compteur): "_"
+        return()
+def ajoutesymbole
+def testeVictoireVerticale
+def testeVictoireHorizontale
+def testeVictoireDiagonale
+
+
+def affichegrille (grille):
+	print(grille)
+	return
+
+#TicTacToe Number 3 
+    
+joueur = 0
+victoire = 0 
+tour = 0 
+
+def grille():
+    print("\n")
+    print("\t     |     |")
+    print("\t  {}  |  {}  |  {}".format([0],[1],[2]))
+    print('\t_____|_____|_____')
+ 
+    print("\t     |     |")
+    print("\t  {}  |  {}  |  {}".format([3],[4],[5]))
+    print('\t_____|_____|_____')
+ 
+    print("\t     |     |")
+ 
+    print("\t  {}  |  {}  |  {}".format([6],[7],[8]))
+    print("\t     |     |")
+    print("\n")
+
+
+while True:
+    grille ()
+    
+def ajoutesymbole (grille,i):
+    if (grille[i]=="X" or grille[i]=="O"):
+        print ("Cette case est déjà occupée")
+    if (grille[i]=="vide" and joueur):
+        grille[i] = "X"
+    if (grille[i]=="vide" and joueur):
